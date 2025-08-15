65de7b62f7bfb4e5e7ac7b3bfc253a01865fd836
Austine247-droid <ugberaeseoa@gmail.com>
2025-08-15 10:54:44 +0000

    first commit

---
+++ components/layout/Header.tsx
@@ -0,0 +1,49 @@
+// import { Images } from "@/constants"
+// import Image from "next/image"
+// import Link from "next/link"
+
+
+
+// const Header = () => {
+//   return (
+//     <header className="flex flex-col">
+//         <div className="bg-[#34967C]">
+//         <div>
+//         <Image src={Images.caseImg} alt="bag" width={24} height={24} />
+//         <p >Overseas trip? Get the latest information on travel guides</p>
+//       <button >More Info</button>
+//       </div>  
+//       </div>
+      
+//       <div>
+//         <Link href="/" className="hidden">
+//         <Image src={Images.logo} alt="logo"  width={60} height={28}/>
+//         </Link>
+//       </div> 
+     
+    
+//     {/* <div className="bg-teal-600 px-4 py-3">
+//   <div className="max-w-6xl mx-auto flex flex-col sm:flex-row items-center justify-between gap-4 text-white">
+//     <div className="flex items-center gap-2">
+//       <span>
+//         {/* Replace with your icon */}
+//         {/* <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+//           <path d="M5 13l4 4L19 7" />
+//         </svg>
+//       </span>
+//       <p className="text-sm sm:text-base">
+//         Overseas trip? Get the latest information on travel guides
+//       </p>
+//     </div>
+
+//     <button >
+//       More Info
+//     </button>
+//   </div>
+// </div> */} 
+
+//     </header>
+//   )
+// }
+
+// export default Header
